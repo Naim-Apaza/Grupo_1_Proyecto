f84da8e680eb967e908ee716b03e13835d27edda
FernandoRuiz17 <138071501+FernandoRuiz17@users.noreply.github.com>
2023-08-07 19:15:10 -0300

    Add files via upload

---
+++ SPRINT 2/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+const PublicFolderPath = path.resolve(__dirname, './Images');
+app.use(express.static(PublicFolderPath));
+
+app.listen(4000, () =>
+    console.log("servidorencendido 4000")
+);
+app.get("/", (req, res) => {
+    res.send("HOME")
+
+
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/Home.html'));
+
+})
