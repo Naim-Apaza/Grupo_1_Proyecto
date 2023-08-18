6924a2c1c13ff43bfa1d70b707668755f97ea2e1
Lautaro Cejas <lauti217@live.com>
2023-08-18 13:07:59 -0300

    Agregado de login y re-estilización

---
+++ controllers/loginController.js
@@ -0,0 +1,9 @@
+// Controlador del login
+
+const controller = {
+    login: (req, res) => {
+      res.render("login");
+    },
+  };
+  
+  module.exports = controller
--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -4,10 +4,12 @@ const express = require("express");
 const router = express.Router();
 const mainController = require('../controllers/mainController')
 const productController = require('../controllers/productController')
+const loginController = require('../controllers/loginController')
 
 // Ruteos
 
 router.get('/', mainController.index)
 router.get('/productDetail', productController.detail)
+router.get('/login', loginController.login)
 
 module.exports = router
