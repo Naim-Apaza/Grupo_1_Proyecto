0fc205467f8d86e14485e092e2025bd096c5b4ca
Lautaro Cejas <lauti217@live.com>
2023-08-21 23:24:55 -0300

    Agregando registro, aun sin completar

---
+++ controllers/registerController.js
@@ -0,0 +1,9 @@
+// Controlador del registro
+
+const controller = {
+    register: (req, res) => {
+      res.render("register");
+    },
+  };
+  
+  module.exports = controller
--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -2,14 +2,19 @@
 
 const express = require("express");
 const router = express.Router();
+
+// Controladores 
+
 const mainController = require('../controllers/mainController')
 const productController = require('../controllers/productController')
 const loginController = require('../controllers/loginController')
+const registerController = require('../controllers/registerController')
 
 // Ruteos
 
 router.get('/', mainController.index)
 router.get('/productDetail', productController.detail)
 router.get('/login', loginController.login)
+router.get('/register', registerController.register)
 
 module.exports = router
