b9b13ea62dd9d03b92bd16a5614891fb6880f60e
roberto3219 <llamparoberto5@gmail.com>
2023-10-05 22:32:00 -0300

    ya estaria

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,25 @@
+const path = require('path')
+const fs = require("fs")
 module.exports={
-    register: function(req,res){
-        
-    }
+    register: (req, res) => {
+        res.render("register");
+      },
+      saveRegister: (req, res) => {
+        let saveImage = req.file;
+        //console.log(saveImage)
+        if (saveImage !== undefined) {
+          let User = req.body;
+          console.log(User);
+          res.redirect("/products");
+        } else {
+          res.render("register");
+        }
+      },
+      login: (req, res) => {
+        res.render("login");
+      },
+      loadLogin: (req, res) => {
+        let save_Login = req.body;
+        res.redirect("/products");
+      },
 }
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,10 +1,27 @@
 const express = require('express')
 const router = express.Router();
 const path = require("path");
+const multer = require("multer")
+const logUserMiddleware=require("../middlewares/logUserMiddleware")
+const UserController = require("../controllers/userController")
+ 
+const multerDiskStorage = multer.diskStorage({
+    destination:(req,file,callback)=>{
+     let folder = path.join(__dirname, '../public/images/user')
+     callback(null, folder)   
+    },
+    filename: (req,file,callback)=>{
+        //console.log(file)
+        let imageName = "user-"+Date.now() + path.extname(file.originalname)
+        callback(null,imageName)
+    } 
+})
 
-const Usercontroller = require("../controllers/userController")
+let fileUpload = multer({ storage:multerDiskStorage}) 
 
-
- router.get("/register",Usercontroller.register)
+router.get("/login", UserController.login);
+router.post("/login",UserController.loadLogin)
+router.get("/register",UserController.register);
+router.post("/register",fileUpload.single("fotocopia"),logUserMiddleware,UserController.saveRegister) 
 
 module.exports=router;
