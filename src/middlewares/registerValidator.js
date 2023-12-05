12631323ffdf414dc968f6f0464a9848c1ab37c6
FernandoRuiz17 <138071501+FernandoRuiz17@users.noreply.github.com>
2023-12-04 20:52:12 -0600

    Validacion Registro

--- src/middlewares/registerValidator.js
+++ src/middlewares/registerValidator.js
@@ -1,31 +1,39 @@
 // Middleware de validacion del registro
 let path = require("path");
 const { body } = require("express-validator");
-
+const { Usuario } = require("../database/models");
 const registerValidator = [
-  body("name").notEmpty().withMessage("debes completar el campo nombre"),
-  body("lastName").notEmpty().withMessage("Debes completar el campo apellido"),
-  body("correo").isEmail().withMessage("debe ser un correo valido"),
-  body("password").notEmpty().withMessage("debe completar el campo contraseña"),
-  body("userImage").custom((value, { req }) => {
-    let file = req.file;
-    let acceptedExtensions = [".jpg", ".png", ".gif"];
-    if (!file) {
-      throw new Error("Tienes que subir una imagen");
-    } else {
-      let fileExtensions = path.extname(file.originalname);
-      if (!acceptedExtensions.includes(fileExtensions)) {
-        throw new Error(
-          "Las extensiones de archivos permitidas son JPG PNG GIF"
-        );
+  body("name")
+    .notEmpty().withMessage("El nombre es obligatorio.")
+    .isLength({ min: 2 }).withMessage("El nombre debe tener al menos 2 caracteres."),
+  body("lastName")
+    .notEmpty().withMessage("El apellido es obligatorio.")
+    .isLength({ min: 2 }).withMessage("El apellido debe tener al menos 2 caracteres."),
+  body("correo")
+    .notEmpty().withMessage("El correo es obligatorio.")
+    .isEmail().withMessage("Debe ser un formato de e-mail válido.")
+    .custom(async (value, { req }) => {
+      const existingUser = await Usuario.findOne({ where: { correo: value } });
+      if (existingUser) {
+        throw new Error("El correo electrónico ya está registrado.");
       }
-    }
-
-    return true;
-  }),
-  body("repass").custom((value, { req }) => {
-    return value === req.body.password;
-  }),
+      return true;
+    }),
+  body("password")
+    .notEmpty().withMessage("La contraseña es obligatoria.")
+    .isLength({ min: 8 }).withMessage("La contraseña debe tener al menos 8 caracteres."),
+  body("userImage")
+    .custom((value, { req }) => {
+      if (!req.file) {
+        throw new Error("Debe subir una imagen.");
+      }
+      const allowedExtensions = [".jpg", ".jpeg", ".png", ".gif"];
+      const fileExtension = path.extname(req.file.originalname).toLowerCase();
+      if (!allowedExtensions.includes(fileExtension)) {
+        throw new Error("La imagen debe tener una extensión válida (JPG, JPEG, PNG, GIF).");
+      }
+      return true;
+    })
 ];
 
 module.exports = registerValidator;
